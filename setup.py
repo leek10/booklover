8726b5c663d1a992dad1a9ad97ce4bf33ac1944d
Kayla Lee <kl2jmx@virginia.edu>
2024-07-02 18:45:17 -0400

    commit

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
-setup(name = 'hw09-booklover',
+setup(name = 'hw09booklover',
       version = '1.0',
       description = 'A package for book lovers to add their books to a read list',
       url = 'https://github.com/leek10/hw09-booklover',
       author = 'Kayla Lee',
       license = 'MIT',
-      packages = ['bookloverpackage'])
+      packages = ['bookloverpackage'],
+      install_requires = ['pandas'])
